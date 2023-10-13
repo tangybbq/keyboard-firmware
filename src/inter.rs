f10acc279660d23d77e0dac17e4e5965dcaf4f89
David Brown <davidb@davidb.org>
2023-10-12 23:00:54 -0600

    Rewrite inter protocol code
    Write the protocol code as its own module with proper encoding and decoding.  It
    is written in a rather "Rusty" way which probably results in quite a bit of
    extra copying.  Consider moving the event queue out into the outer structure so
    that it doesn't have to be copied around until the end.

--- src/inter.rs
+++ src/inter.rs
@@ -3,28 +3,26 @@
 // At this point, we're just using the rp2040_hal UART type directly.
 
 use arraydeque::ArrayDeque;
-use crc::{Crc, CRC_16_IBM_SDLC};
-use defmt::{warn, info};
+use defmt::warn;
 use embedded_hal::serial::Read;
 use sparkfun_pro_micro_rp2040::hal;
 use sparkfun_pro_micro_rp2040::hal::uart::UartPeripheral;
 
 use crate::Side;
 
-pub const CRC: Crc<u16> = Crc::<u16>::new(&CRC_16_IBM_SDLC);
+use self::serialize::{PacketBuffer, Decoder, Packet};
+
+mod serialize;
 
 pub struct InterHandler<D, P>
     where D: hal::uart::UartDevice, P: hal::uart::ValidUartPinout<D>
 {
     uart: UartPeripheral<hal::uart::Enabled, D, P>,
     // state: State,
-    xmit_buffer: ArrayDeque<u8, 32>,
-    recv_buffer: ArrayDeque<u8, 32>,
-    crc_pos: u8,
-    crc_low: u8,
-    crc_high: u8,
-    seq: u16,
+    xmit_buffer: PacketBuffer,
+    receiver: Decoder,
     side: Side,
+    seq: u8,
 }
 
 impl<D: hal::uart::UartDevice, P: hal::uart::ValidUartPinout<D>>
@@ -35,11 +33,8 @@ impl<D: hal::uart::UartDevice, P: hal::uart::ValidUartPinout<D>>
             uart,
             // state: State::Idle,
             xmit_buffer: ArrayDeque::new(),
+            receiver: Decoder::new(),
             // TODO: Can we come up with a more distinct sequence number?
-            recv_buffer: ArrayDeque::new(),
-            crc_pos: 0,
-            crc_low: 0,
-            crc_high: 0,
             seq: 1,
             side,
         }
@@ -58,18 +53,7 @@ impl<D: hal::uart::UartDevice, P: hal::uart::ValidUartPinout<D>>
         }
 
         // Build a new packet.  Note that it is already empty.
-
-        // Idle header.
-        self.xmit_buffer.push_back(Token::new_start(self.side) as u8).unwrap();
-        push_u16(&mut self.xmit_buffer, self.seq);
-        self.seq += 1;
-        if self.seq > 0x3fff {
-            self.seq = 0;
-        }
-
-        // End of packet, with CRC.
-        self.xmit_buffer.push_back(Token::EOP as u8).unwrap();
-        push_crc(&mut self.xmit_buffer);
+        Packet::Idle { side: self.side }.encode(&mut self.xmit_buffer, &mut self.seq);
 
         self.try_send();
     }
@@ -100,47 +84,16 @@ impl<D: hal::uart::UartDevice, P: hal::uart::ValidUartPinout<D>>
                     0x80
                 }
             };
-            match Token::from_byte(byte) {
-                Conversion::Token(tok) => {
-                    if tok.is_start() {
-                        self.recv_buffer.clear();
+            if let Some(packet) = self.receiver.add_byte(byte) {
+                match packet {
+                    Packet::Idle { side } => {
+                        if side == self.side {
+                            warn!("Both parts are the same side")
+                        }
                     }
-                    let _ = self.recv_buffer.push_back(tok as u8);
-                    if tok == Token::EOP {
-                        self.crc_pos = 1;
-                    } else {
-                        self.crc_pos = 0;
+                    Packet::Primary { side: _, led: _ } => {
                     }
-                }
-                Conversion::Invalid => {
-                    warn!("Invalid byte received");
-                }
-                Conversion::Data(d) => {
-                    match self.crc_pos {
-                        0 => {
-                            let _ = self.recv_buffer.push_back(d);
-                        }
-                        1 => {
-                            self.crc_low = d;
-                            self.crc_pos = 2;
-                        }
-                        2 => {
-                            self.crc_high = d;
-                            self.crc_pos = 3;
-
-                            let crc = get_crc(&self.recv_buffer);
-                            let crc = crc & 0x3fff;
-                            if crc == ((self.crc_high as u16) << 7) | (self.crc_low as u16) {
-                                // Received packet.
-                                info!("Good");
-                            } else {
-                                info!("Bad: crc:{:x} high:{:x} low:{:x}", crc, self.crc_high, self.crc_low);
-                                for b in &self.recv_buffer {
-                                    info!("byte: {:x}", *b);
-                                }
-                            }
-                        }
-                        _ => (),
+                    Packet::Secondary { side: _, keys: _ } => {
                     }
                 }
             }
@@ -148,81 +101,3 @@ impl<D: hal::uart::UartDevice, P: hal::uart::ValidUartPinout<D>>
     }
 }
 
-/// Push a 14-bit bit value, discarding the top two bits.  Happens little-endian.
-fn push_u16<const CAP: usize>(buf: &mut ArrayDeque<u8, CAP>, value: u16) {
-    buf.push_back((value & 0x7f) as u8).unwrap();
-    buf.push_back(((value >> 7) & 0x7f) as u8).unwrap();
-}
-
-/// Push the low 14-bits of a CRC of the buffer so far.
-fn push_crc<const CAP: usize>(buf: &mut ArrayDeque<u8, CAP>) {
-    let mut digest = CRC.digest();
-    let (a, b) = buf.as_slices();
-    digest.update(a);
-    digest.update(b);
-    let crc = digest.finalize();
-    push_u16(buf, crc);
-}
-
-/// Get the CRC of the buffer.
-fn get_crc<const CAP: usize>(buf: &ArrayDeque<u8, CAP>) -> u16 {
-    let mut digest = CRC.digest();
-    let (a, b) = buf.as_slices();
-    digest.update(a);
-    digest.update(b);
-    digest.finalize()
-}
-
-/*
-enum State {
-    Idle,
-    Primary,
-    Secondary,
-}
-*/
-
-#[repr(u8)]
-#[derive(PartialEq, Eq, Clone, Copy)]
-enum Token {
-    // 0x81 seqlo seqhi
-    IdleLeft = 0x81,
-    // 0x81 seqlo seqhi
-    IdleRight = 0x82,
-
-    // 0xf0 crclo crchi
-    EOP = 0xf0,
-}
-
-enum Conversion {
-    Token(Token),
-    Invalid,
-    Data(u8),
-}
-
-impl Token {
-    fn is_start(self) -> bool {
-        match self {
-            Token::IdleLeft => true,
-            Token::IdleRight => true,
-            _ => false,
-        }
-    }
-
-    fn new_start(side: Side) -> Token {
-        if side.is_left() {
-            Token::IdleLeft
-        } else {
-            Token::IdleRight
-        }
-    }
-
-    fn from_byte(byte: u8) -> Conversion {
-        match byte {
-            0x81 => Conversion::Token(Token::IdleLeft),
-            0x82 => Conversion::Token(Token::IdleRight),
-            0xf0 => Conversion::Token(Token::EOP),
-            b if b >= 0x80 => Conversion::Invalid,
-            b => Conversion::Data(b),
-        }
-    }
-}
---
+++ src/inter/serialize.rs
@@ -0,0 +1,323 @@
+//! Serialization of the inter protocol.
+
+use core::mem::replace;
+
+use arraydeque::ArrayDeque;
+use arrayvec::ArrayVec;
+use crc::{Crc, CRC_16_IBM_SDLC, Digest};
+use defmt::warn;
+use smart_leds::RGB8;
+
+// TODO: Make the hardcoded sizes part of the board support.
+
+use crate::{Side, matrix::KeyEvent};
+
+pub type PacketBuffer = ArrayDeque<u8, 28>;
+pub type EventVec = ArrayVec<KeyEvent, 21>;
+
+/// The CRC generator we are using.
+pub const CRC: Crc<u16> = Crc::<u16>::new(&CRC_16_IBM_SDLC);
+
+pub enum Packet {
+    /// The idle packet is sent before we know anything about which side of the
+    /// channel we are on.
+    Idle {
+        /// Which part of the keyboard we are.
+        side: Side,
+    },
+    /// The "primary" packet is the side connected to the USB port.
+    Primary {
+        /// Which side we are.
+        side: Side,
+        /// Set the LEDs to this value (probably should be more of a state)
+        led: RGB8,
+    },
+    Secondary {
+        /// Which side of the keyboard we are.
+        side: Side,
+        /// Key events.
+        keys: EventVec,
+    },
+}
+
+/// The version of the protocol described by this code.  It is a fatal error for
+/// there to be a mismatch.  This is a value less than 128.
+//const VERSION: u8 = 1;
+
+// The protocol itself is byte-oriented.  The high bit indicates whether or not
+// this is a control byte, the 0x40 bit indicates which side this packet
+// originates from.  The other bits indicate the particular value being sent.
+// This initial version only has three bytes:
+//
+// 1 - Indicates idle.  (0x81 and 0xc1)
+//     followed by a 7-bit sequence number.  This could be used to help diagnose
+//     dropped packets.
+// 2 - Primary (0x82 and 0xc2)
+//     7-bit sequence number
+//     3 7-bit numbers of RGB8 values.  The low bit of the intensity is
+//     discarded.
+// 3 - Slave (0x83 and 0xc3)
+//     7-bit sequence number
+//     7-bit bytes of key events
+// 3f - CRC (0xff) (note that the CRC is the same for either side)
+//     2 7-bit bytes, representing the low 14 bits of the CRC16
+//
+// The key events are encoded as the low 6 bits (0-63) are the key number, and
+// the next bit (64) indicates that this is a release event.
+
+impl Packet {
+    /// Encode this packet for the on stream.  The encoding will be placed in
+    /// the given buffer.
+    pub fn encode(&self, buf: &mut PacketBuffer, seq: &mut u8) {
+        buf.clear();
+        match self {
+            Packet::Idle { side } => {
+                buf.push_back(token(1, *side)).unwrap();
+                buf.push_back(*seq).unwrap();
+            }
+            Packet::Primary { side, led } => {
+                buf.push_back(token(2, *side)).unwrap();
+                buf.push_back(*seq).unwrap();
+                buf.push_back(led.r >> 1).unwrap();
+                buf.push_back(led.g >> 1).unwrap();
+                buf.push_back(led.b >> 1).unwrap();
+            }
+            Packet::Secondary { side, keys } => {
+                buf.push_back(token(2, *side)).unwrap();
+                buf.push_back(*seq).unwrap();
+                for elt in keys {
+                    let b = match elt {
+                        KeyEvent::Press(code) => *code,
+                        KeyEvent::Release(code) => *code | 64,
+                    };
+                    buf.push_back(b).unwrap();
+                }
+            }
+        }
+
+        // Compute and encode the CRC.
+        buf.push_back(0xff).unwrap();
+        let (a, b) = crc_split(get_crc(buf));
+        buf.push_back(a).unwrap();
+        buf.push_back(b).unwrap();
+
+        let tmp = seq.wrapping_add(1);
+        *seq = if tmp < 0x80 { tmp } else { 0 };
+    }
+}
+
+/// A packet decoder.  This maintains all the necessary internal state to decode
+/// incoming packets, and return them when they are ready.
+pub struct Decoder {
+    state: DecodeState,
+}
+
+impl Decoder {
+    pub fn new() -> Decoder {
+        Decoder {
+            state: DecodeState::Init,
+        }
+    }
+
+    /// Handle another incoming byte.  Returns a decoded packet when completed.
+    pub fn add_byte(&mut self, byte: u8) -> Option<Packet> {
+        // Is this a CRC indicator.
+        if byte == 0xff {
+            let state = replace(&mut self.state, DecodeState::Init);
+            self.state = state.start_crc();
+            return None;
+        }
+        // Check for tokens.  These will already reset to this "first state"
+        if (byte & 0x80) != 0 {
+            self.state = DecodeState::First { token: byte };
+            return None;
+        }
+
+        // Otherwise deal with the data.  This isn't a full match because we
+        // want to move out of some states (potentially), and use the data in
+        // place in others.
+        if let DecodeState::First { token } = self.state {
+            let side = if (token & 0x40) == 0 { Side::Left } else { Side::Right };
+            let mut crc = CRC.digest();
+            let inner = match byte & 0x3f {
+                1 => Some(InnerDecodeState::Idle),
+                2 => Some(InnerDecodeState::Primary {
+                    leds: [0, 0, 0],
+                    pos: 0,
+                }),
+                3 => Some(InnerDecodeState::Secondary {
+                    events: ArrayVec::new(),
+                }),
+                // Otherwise, invalid, start over.
+                _ => None,
+            };
+            match inner {
+                None => self.state = DecodeState::Init,
+                Some(s) => self.state = {
+                    // Note that we basically ignore the sequence number, other than
+                    // using it for the CRC.
+                    crc.update(&[token, byte]);
+                    DecodeState::Inside {
+                        inner: s,
+                        crc: crc,
+                        side: side,
+                    }
+                }
+            }
+            return None;
+        }
+        if let DecodeState::Init = self.state {
+            return None;
+        }
+        if let DecodeState::Inside { inner, crc: _, side: _ } = &mut self.state {
+            inner.decode(byte);
+            return None;
+        }
+
+        // Otherwise, we are in the CRC state.
+        let (packet, done) = if let DecodeState::CRC { inner, expected_crc, gotten, pos, side } = &mut self.state {
+            gotten[*pos] = byte;
+            *pos += 1;
+
+            if *pos == 2 {
+                if *expected_crc == (gotten[0], gotten[1]) {
+                    // The packet is valid.
+                    // TODO: This clone is excessive copying.
+                    (Some(inner.clone().into_packet(*side)), true)
+                } else {
+                    warn!("Invalid CRC received");
+                    (None, true)
+                }
+            } else {
+                (None, false)
+            }
+        } else {
+            panic!("State error");
+        };
+        if done {
+            self.state = DecodeState::Init;
+        }
+        packet
+    }
+}
+
+/// Outer decoder state.
+enum DecodeState {
+    /// Between packets.
+    Init,
+    /// We got a token, but don't yet have the sequence number.
+    First {
+        token: u8,
+    },
+    /// We got the token and are inside the packet.
+    Inside {
+        inner: InnerDecodeState,
+        crc: Digest<'static, u16>,
+        side: Side,
+    },
+    /// We got the CRC header, waiting for the two bytes of the CRC.
+    CRC {
+        inner: InnerDecodeState,
+        expected_crc: (u8, u8),
+        gotten: [u8; 2],
+        pos: usize,
+        side: Side,
+    },
+}
+
+impl DecodeState {
+    fn start_crc(self) -> Self {
+        match self {
+            DecodeState::Inside { inner, mut crc, side } => {
+                crc.update(&[0xff]);
+                let result = crc.finalize();
+                DecodeState::CRC {
+                    inner: inner,
+                    expected_crc: crc_split(result),
+                    gotten: [0, 0],
+                    pos: 0,
+                    side,
+                }
+            }
+            _ => {
+                // If we aren't excpecting a CRC, just reset back to the
+                // Init state.
+                DecodeState::Init
+            }
+        }
+    }
+}
+
+/// Internal decoder state, once we know what token this is.
+#[derive(Clone)]
+enum InnerDecodeState {
+    Idle,
+    /// Primary token and sequence received.  Waiting for the 3 LED values.
+    Primary {
+        /// Digest so far.
+        leds: [u8; 3],
+        /// Position within the components.
+        pos: usize,
+    },
+    Secondary {
+        /// Digest so far.
+        events: EventVec,
+    },
+}
+
+impl InnerDecodeState {
+    fn decode(&mut self, byte: u8) {
+        match self {
+            // This is invalid, but just ignore.
+            InnerDecodeState::Idle => (),
+            // Primary, stores the LED values.
+            InnerDecodeState::Primary { leds, pos } => {
+                if *pos < 3 {
+                    leds[*pos] = byte << 1;
+                    *pos += 1;
+                }
+                // If past end, just discard.
+            }
+            InnerDecodeState::Secondary { events } => {
+                let ev = if (byte & 64) == 0 {
+                    KeyEvent::Press(byte)
+                } else {
+                    KeyEvent::Release(byte & 63)
+                };
+                events.push(ev);
+            }
+        }
+    }
+
+    fn into_packet(self, side: Side) -> Packet {
+        match self {
+            InnerDecodeState::Idle => Packet::Idle { side },
+            InnerDecodeState::Primary { leds, pos: _ } => {
+                let led = RGB8::new(leds[0], leds[1], leds[2]);
+                Packet::Primary { side, led }
+            }
+            InnerDecodeState::Secondary { events } => Packet::Secondary { side, keys: events }
+        }
+    }
+}
+
+/// Calculate the CRC of the contents of the buffer.  Note that we only use the
+/// low 14-bits of the CRC.
+fn get_crc(buf: &PacketBuffer) -> u16 {
+    let mut digest = CRC.digest();
+    let (a, b) = buf.as_slices();
+    digest.update(a);
+    digest.update(b);
+    digest.finalize()
+}
+
+fn crc_split(crc: u16) -> (u8, u8) {
+    ((crc & 0x7f) as u8, ((crc >> 7) & 0x7f) as u8)
+}
+
+fn token(code: u8, side : Side) -> u8 {
+    0x80 | code | (match side {
+        Side::Left => 0x00,
+        Side::Right => 0x40,
+    })
+}
