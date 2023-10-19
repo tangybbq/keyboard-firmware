05c85b22ddf847c17eace32811958ecc1d69d46e
David Brown <davidb@davidb.org>
2023-10-19 10:48:46 -0600

    Implement mode selector for LayoutManager
    Handle the toggle version of the mode selector key.  This currently switches
    between raw steno mode and an unimplemented Artsey mode.

--- bbq-keyboard/src/layout.rs
+++ bbq-keyboard/src/layout.rs
@@ -4,6 +4,8 @@
 //! - Steno dictionary conversion
 //! - All of the interaction between these.
 
+use crate::log::info;
+
 use crate::{KeyEvent, EventQueue};
 
 use self::steno::RawStenoHandler;
@@ -44,16 +46,27 @@ mod steno;
 // between some of the major modes. This is frequent enough that I've given it a
 // dedicated key. If that key is pressed by itself, or as a chord with a small
 // number of other keys, the mode will be set to the specified mode.
+//
+// There is a mode switch key that is handled specially by this layer. It can be
+// pressed by itself, which will cycle through the modes. Or, there will be a
+// key that can be pressed after it (hold and tap) to select a specific mode.
+// Keys sent between the first tap of the mode key and it's release aren't sent
+// to the lower layers. Press mode select while other keys are pressed will have
+// noeffect.
 
 /// The layout manager.
 pub struct LayoutManager {
     raw: steno::RawStenoHandler,
+
+    // Global mode.  This indicates what mode we are in.
+    mode: ModeSelector,
 }
 
 impl LayoutManager {
     pub fn new() -> Self {
         LayoutManager {
             raw: RawStenoHandler::new(),
+            mode: ModeSelector::default(),
         }
     }
 
@@ -68,6 +81,118 @@ impl LayoutManager {
 
     /// Handle a single key event.
     pub fn handle_event(&mut self, event: KeyEvent, events: &mut EventQueue) {
-        self.raw.handle_event(event, events);
+        if self.mode.event(event) {
+            match self.mode.get() {
+                LayoutMode::Artsey => info!("TODO: Artsey"),
+                LayoutMode::Steno => {
+                    self.raw.handle_event(event, events);
+                }
+            }
+        }
+    }
+}
+
+/// The global keyboard mode.
+#[derive(Clone, Copy)]
+enum LayoutMode {
+    Steno,
+    Artsey,
+}
+
+impl Default for LayoutMode {
+    /// The initial mode we're starting in.
+    fn default() -> Self {
+        LayoutMode::Steno
+    }
+}
+
+/// A layout mode manager handles the behavior of the special key.
+struct ModeSelector {
+    /// The current mode.  If this is None, then we waiting to determine the mode we're in.
+    mode: LayoutMode,
+
+    /// Selecting mode.  Once the special key is seen, we start ignoring keys.
+    selecting: bool,
+
+    /// Track keys that are currently pressed.
+    pressed: u64,
+
+    /// When we see the layout mode, pressed keys will register here.
+    seen: u64,
+}
+
+impl Default for ModeSelector {
+    fn default() -> Self {
+        ModeSelector {
+            mode: LayoutMode::default(),
+            selecting: false,
+            pressed: 0,
+            seen: 0,
+        }
+    }
+}
+
+impl ModeSelector {
+    /// Get the current mode.
+    fn get(&self) -> LayoutMode {
+        self.mode
+    }
+
+    /// Handle a keyevent, and return 'true' if the key even should be passed down to lower layers.
+    fn event(&mut self, event: KeyEvent) -> bool {
+        // Update the mask of keys that have been pressed.
+        match event {
+            KeyEvent::Press(k) => self.pressed |= 1 << k,
+            KeyEvent::Release(k) => self.pressed &= !(1 << k),
+        }
+
+        // If we've pressed the mode selector, enter the funny mode.
+        if let KeyEvent::Press(13) = event {
+            // Only do something here if either we are selecting, or no other
+            // keys have been pressed.
+            if self.selecting || (self.pressed & !(1 << 13)) == 0 {
+                // Toggle the mode.
+                self.mode = self.mode.next();
+                self.selecting = true;
+            }
+        }
+
+        // Special case for selecting.
+        if self.selecting {
+            // Merge in any keys seen.
+            self.seen |= self.pressed;
+
+            // When evertything is released, pick our next mode.
+            if self.pressed == 0 {
+                // TODO: Look at 'seen' to determine fixed mode changes. For
+                // now, just do toggle.
+                self.seen = 0;
+                self.selecting = false;
+                info!("Mode change: {:?}", self.mode);
+            }
+            false
+        } else {
+            // If not selecting, just handle in layer below.
+            true
+        }
+    }
+}
+
+impl LayoutMode {
+    /// Move to the next mode.
+    fn next(self) -> Self {
+        match self {
+            LayoutMode::Steno => LayoutMode::Artsey,
+            LayoutMode::Artsey => LayoutMode::Steno,
+        }
+    }
+}
+
+impl defmt::Format for LayoutMode {
+    fn format(&self, fmt: defmt::Formatter) {
+        match self {
+            LayoutMode::Steno => defmt::write!(fmt, "steno"),
+            LayoutMode::Artsey => defmt::write!(fmt, "artsey"),
+        }
     }
 }
