12c9465a115bcd2da2116420b27058e89b99e2cf
David Brown <davidb@davidb.org>
2023-10-29 16:25:59 -0600

    proto: Increase heap size to 8k
    Double the size of the heap to 8k.  The steno dict stuff seems to settle on
    around 2kb of allocated data while it is running.  Make this sufficiently large
    to avoid any fragmentation issues.

--- proto/src/main.rs
+++ proto/src/main.rs
@@ -67,7 +67,7 @@ pub static BOOT_LOADER: [u8; 256] = rp2040_boot2::BOOT_LOADER_W25Q080;
 #[entry]
 fn main() -> ! {
     use core::mem::MaybeUninit;
-    const HEAP_SIZE: usize = 4096;
+    const HEAP_SIZE: usize = 8192;
     static mut HEAP_MEM: [MaybeUninit<u8>; HEAP_SIZE] = [MaybeUninit::uninit(); HEAP_SIZE];
     unsafe { HEAP.init(HEAP_MEM.as_ptr() as usize, HEAP_SIZE) }
 
