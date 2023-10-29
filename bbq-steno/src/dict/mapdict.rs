028453a293d830bdabc86da1452c276166a6fc2e
David Brown <davidb@davidb.org>
2023-10-28 22:02:25 -0600

    dict: Move code into modules
    Move some more of the types into their own modules.

---
+++ bbq-steno/src/dict/mapdict.rs
@@ -0,0 +1,48 @@
+//! Simple dictionary implemented with maps.
+
+use alloc::collections::BTreeMap;
+use alloc::vec::Vec;
+
+use crate::Stroke;
+use super::Dict;
+
+extern crate alloc;
+
+/// A simple dictionary implementation.  This is implemented by storing Vecs of
+/// the keys, and Strings as the values.
+pub struct MapDict {
+    map: BTreeMap<Vec<Stroke>, String>,
+    longest: usize,
+}
+
+/// A dictionary builder.
+pub struct MapDictBuilder {
+    map: BTreeMap<Vec<Stroke>, String>,
+}
+
+impl Dict for MapDict {
+    fn lookup<'a>(&'a self, query: &[Stroke]) -> Option<&'a str> {
+        self.map.get(query).map(|s| s.as_ref())
+    }
+
+    fn longest_key(&self) -> usize {
+        self.longest
+    }
+}
+
+impl MapDictBuilder {
+    pub fn new() -> MapDictBuilder {
+        MapDictBuilder { map: BTreeMap::new() }
+    }
+
+    /// Insert a definition.
+    pub fn insert(&mut self, key: Vec<Stroke>, definition: String) {
+        self.map.insert(key, definition);
+    }
+
+    /// Freeze the dictionary.
+    pub fn into_map_dict(self) -> MapDict {
+        let longest = self.map.keys().map(|k| k.len()).max().unwrap_or(0);
+        MapDict { map: self.map, longest }
+    }
+}
