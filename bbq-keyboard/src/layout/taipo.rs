44389b1d82ee262cc2f183e8e5324ba04d53d1b7
David Brown <davidb@davidb.org>
2024-02-04 16:36:19 -0700

    taipo: Implement many keys
    Implement all of the letter keys and their shifted and symbol variants.

--- bbq-keyboard/src/layout/taipo.rs
+++ bbq-keyboard/src/layout/taipo.rs
@@ -9,18 +9,29 @@
 //! the halves.  As such, we have to maintain the state of the two halves
 //! separately.
 
+use arraydeque::ArrayDeque;
+use usbd_human_interface_device::page::Keyboard;
+
 use crate::log::info;
 
-use crate::{EventQueue, KeyEvent, Side};
+use crate::{EventQueue, KeyEvent, Side, Mods, KeyAction, Event};
 
 pub struct TaipoManager {
+    /// Managing state for each side.
     sides: [SideManager; 2],
+    /// Key events passed through.
+    keys: TaipoEvents,
+
+    /// Modifiers that are down.
+    oneshot: Mods,
 }
 
 impl Default for TaipoManager {
     fn default() -> Self {
         TaipoManager {
             sides: [Default::default(), Default::default()],
+            keys: TaipoEvents::new(),
+            oneshot: Mods::empty(),
         }
     }
 }
@@ -32,8 +43,41 @@ impl TaipoManager {
 
     /// Tick is needed to track time.
     pub fn tick(&mut self, events: &mut dyn EventQueue) {
-        self.sides[0].tick();
-        self.sides[1].tick();
+        self.sides[0].tick(&mut self.keys);
+        self.sides[1].tick(&mut self.keys);
+
+        // After polling, handle any events.
+        while let Some(tevent) = self.keys.pop_front() {
+            // info!("ev: p:{}, code:{:x}", tevent.is_press, tevent.code);
+            if !tevent.is_press {
+                // For now, release the keys immediately.  This resolves the
+                // issue with rollover of the same key, and avoids having to
+                // track things.  This may have to change if we require multiple
+                // letters to be pressed (GUI apps might do this, but it is uncommon).
+                continue;
+            }
+            // Look up the code to see if we have an action.
+            let mut sent = false;
+            match TAIPO_ACTIONS.iter().find(|e| e.code == tevent.code) {
+                Some(Entry { action: Action::Simple(k), .. }) => {
+                    sent = true;
+                    events.push(Event::Key(KeyAction::KeyPress(*k, self.oneshot)));
+                    self.oneshot = Mods::empty();
+                }
+                Some(Entry { action: Action::Shifted(k), .. }) => {
+                    sent = true;
+                    events.push(Event::Key(KeyAction::KeyPress(*k, self.oneshot | Mods::SHIFT)));
+                    self.oneshot = Mods::empty();
+                }
+                Some(Entry { action: Action::OneShot(m), .. }) => {
+                    self.oneshot |= *m;
+                }
+                None => (),
+            }
+            if sent {
+                events.push(Event::Key(KeyAction::KeyRelease));
+            }
+        }
         let _ = events;
     }
 
@@ -59,7 +103,7 @@ impl TaipoManager {
         if is_press {
             self.sides[side.index()].press(*tcode);
         } else {
-            self.sides[side.index()].release(*tcode);
+            self.sides[side.index()].release(*tcode, &mut self.keys);
         }
         let _ = events;
     }
@@ -97,34 +141,46 @@ impl SideManager {
         // info!("Usmpress: down:{} seen:{}, age:{}", self.down, self.seen, self.age);
     }
 
-    fn release(&mut self, tcode: u16) {
+    fn release(&mut self, tcode: u16, keys: &mut TaipoEvents) {
         // info!("smrel: down:{} seen:{}, age:{}", self.down, self.seen, self.age);
         self.pressed &= !tcode;
         // If everything is released, and the timer hasn't expired, we need to
         // send down, and then release.
         if self.pressed == 0 {
             if !self.down {
-                info!("taipo: press {:x}", self.seen);
+                let _ = keys.push_back(TaipoEvent { is_press: true, code: self.seen });
+                // info!("taipo: press {:x}", self.seen);
             }
-            info!("taipo: release {:x}", self.seen);
+            let _ = keys.push_back(TaipoEvent { is_press: false, code: self.seen });
+            // info!("taipo: release {:x}", self.seen);
             *self = Default::default();
         }
         // info!("Usmrel: down:{} seen:{}, age:{}", self.down, self.seen, self.age);
 
     }
 
-    fn tick(&mut self) {
+    fn tick(&mut self, keys: &mut TaipoEvents) {
         // If we already sent, or just if nothing has been pressed.
         if self.down || self.seen == 0 {
             return;
         }
         self.age = self.age.saturating_add(1);
         if self.age >= 50 {
-            info!("taipo: tpress {:x}", self.seen);
+            let _ = keys.push_back(TaipoEvent { is_press: true, code: self.seen });
+            // info!("taipo: tpress {:x}", self.seen);
             self.down = true;
         }
     }
 }
+
+/// A single press or release indicated by Taipo.
+struct TaipoEvent {
+    is_press: bool,
+    code: u16,
+}
+
+/// A queue of events recorded.
+type TaipoEvents = ArrayDeque<TaipoEvent, 8>;
 
 /// Mapping between scan codes, and Taipo codes.  Taipo codes are a 10 number,
 /// with the top two bits as the two thumb keys, then the top row, and bottom
@@ -197,4 +253,113 @@ static SCAN_MAP: [Option<(Side, u16)>; 48] = [
     None,
     None,
     Some((Side::Right, 0x200)),
+];
+
+/// An Action is what should happen when particular key or combo is pressed.
+/// Taipo does not have anything that acts as a shift key, as all keys are
+/// pressed together (like steno).
+enum Action {
+    Simple(Keyboard),
+    Shifted(Keyboard),
+    OneShot(Mods),
+}
+
+/// The mapping between each key and its Action.
+struct Entry {
+    code: u16,
+    action: Action,
+}
+
+static TAIPO_ACTIONS: [Entry; 70] = [
+    // The single letters, with shift. No outer thumb actions on these
+    // (available?)
+    Entry { code: 0x001, action: Action::Simple(Keyboard::A), },
+    Entry { code: 0x101, action: Action::Shifted(Keyboard::A), },
+    Entry { code: 0x002, action: Action::Simple(Keyboard::O), },
+    Entry { code: 0x102, action: Action::Shifted(Keyboard::O), },
+    Entry { code: 0x004, action: Action::Simple(Keyboard::T), },
+    Entry { code: 0x104, action: Action::Shifted(Keyboard::T), },
+    Entry { code: 0x008, action: Action::Simple(Keyboard::E), },
+    Entry { code: 0x108, action: Action::Shifted(Keyboard::E), },
+    Entry { code: 0x010, action: Action::Simple(Keyboard::R), },
+    Entry { code: 0x110, action: Action::Shifted(Keyboard::R), },
+    Entry { code: 0x020, action: Action::Simple(Keyboard::S), },
+    Entry { code: 0x120, action: Action::Shifted(Keyboard::S), },
+    Entry { code: 0x040, action: Action::Simple(Keyboard::N), },
+    Entry { code: 0x140, action: Action::Shifted(Keyboard::N), },
+    Entry { code: 0x080, action: Action::Simple(Keyboard::I), },
+    Entry { code: 0x180, action: Action::Shifted(Keyboard::I), },
+
+    // Paired letters, shifted, and number/symbol.
+    Entry { code: 0x0c0, action: Action::Simple(Keyboard::Y), },
+    Entry { code: 0x1c0, action: Action::Shifted(Keyboard::Y), },
+    Entry { code: 0x2c0, action: Action::Simple(Keyboard::Keyboard5), },
+
+    Entry { code: 0x00c, action: Action::Simple(Keyboard::H), },
+    Entry { code: 0x10c, action: Action::Shifted(Keyboard::H), },
+    Entry { code: 0x20c, action: Action::Simple(Keyboard::Keyboard0), },
+
+    Entry { code: 0x006, action: Action::Simple(Keyboard::U), },
+    Entry { code: 0x106, action: Action::Shifted(Keyboard::U), },
+    Entry { code: 0x206, action: Action::Simple(Keyboard::Keyboard2), },
+
+    Entry { code: 0x009, action: Action::Simple(Keyboard::D), },
+    Entry { code: 0x109, action: Action::Shifted(Keyboard::D), },
+    Entry { code: 0x209, action: Action::Shifted(Keyboard::Keyboard2), },
+
+    Entry { code: 0x0a0, action: Action::Simple(Keyboard::F), },
+    Entry { code: 0x1a0, action: Action::Shifted(Keyboard::F), },
+    Entry { code: 0x2a0, action: Action::Simple(Keyboard::Keyboard6), },
+
+    Entry { code: 0x00a, action: Action::Simple(Keyboard::C), },
+    Entry { code: 0x10a, action: Action::Shifted(Keyboard::C), },
+    Entry { code: 0x20a, action: Action::Simple(Keyboard::Keyboard1), },
+
+    Entry { code: 0x082, action: Action::Simple(Keyboard::K), },
+    Entry { code: 0x182, action: Action::Shifted(Keyboard::K), },
+    Entry { code: 0x282, action: Action::Shifted(Keyboard::Equal), },
+
+    Entry { code: 0x041, action: Action::Simple(Keyboard::J), },
+    Entry { code: 0x141, action: Action::Shifted(Keyboard::J), },
+    Entry { code: 0x241, action: Action::Simple(Keyboard::Equal), },
+
+    Entry { code: 0x081, action: Action::Simple(Keyboard::W), },
+    Entry { code: 0x181, action: Action::Shifted(Keyboard::W), },
+    Entry { code: 0x281, action: Action::Shifted(Keyboard::Keyboard7), },
+
+    Entry { code: 0x030, action: Action::Simple(Keyboard::B), },
+    Entry { code: 0x130, action: Action::Shifted(Keyboard::B), },
+    Entry { code: 0x230, action: Action::Simple(Keyboard::Keyboard9), },
+
+    Entry { code: 0x003, action: Action::Simple(Keyboard::L), },
+    Entry { code: 0x103, action: Action::Shifted(Keyboard::L), },
+    Entry { code: 0x203, action: Action::Simple(Keyboard::Keyboard4), },
+
+    Entry { code: 0x060, action: Action::Simple(Keyboard::P), },
+    Entry { code: 0x160, action: Action::Shifted(Keyboard::P), },
+    Entry { code: 0x260, action: Action::Simple(Keyboard::Keyboard7), },
+
+    Entry { code: 0x090, action: Action::Simple(Keyboard::G), },
+    Entry { code: 0x190, action: Action::Shifted(Keyboard::G), },
+    Entry { code: 0x290, action: Action::Shifted(Keyboard::Keyboard3), },
+
+    Entry { code: 0x050, action: Action::Simple(Keyboard::Z), },
+    Entry { code: 0x150, action: Action::Shifted(Keyboard::Z), },
+    Entry { code: 0x250, action: Action::Simple(Keyboard::Keyboard8), },
+
+    Entry { code: 0x005, action: Action::Simple(Keyboard::Q), },
+    Entry { code: 0x105, action: Action::Shifted(Keyboard::Q), },
+    Entry { code: 0x205, action: Action::Simple(Keyboard::Keyboard3), },
+
+    Entry { code: 0x014, action: Action::Simple(Keyboard::X), },
+    Entry { code: 0x114, action: Action::Shifted(Keyboard::X), },
+    Entry { code: 0x214, action: Action::Shifted(Keyboard::Keyboard6), },
+
+    Entry { code: 0x028, action: Action::Simple(Keyboard::V), },
+    Entry { code: 0x128, action: Action::Shifted(Keyboard::V), },
+    Entry { code: 0x228, action: Action::Shifted(Keyboard::Keyboard8), },
+
+    Entry { code: 0x018, action: Action::Simple(Keyboard::M), },
+    Entry { code: 0x118, action: Action::Shifted(Keyboard::M), },
+    Entry { code: 0x218, action: Action::Shifted(Keyboard::Keyboard4), },
 ];
