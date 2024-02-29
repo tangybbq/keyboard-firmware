89d1a8acfa0738c91d9b995922bec4e1c628c469
David Brown <davidb@davidb.org>
2024-02-29 14:50:12 -0700

    zbbq: API to the LED strip driver
    This updates the API to allow the LED strip driver to be used.  The API is safe,
    but getting the initial struct is not, due to the inherent unsafety of the
    Zephyr interface.

--- zbbq/src/lib.rs
+++ zbbq/src/lib.rs
@@ -2,11 +2,10 @@
 
 extern crate alloc;
 
-use core::ffi::c_char;
+use core::{panic::PanicInfo, slice};
 
-use core::panic::PanicInfo;
-
-use zephyr_sys::ZephyrAllocator;
+use alloc::format;
+use zephyr_sys::{ZephyrAllocator, Device, LedRgb, LedStripDriver};
 
 use crate::zephyr::message;
 
@@ -20,22 +19,41 @@ fn panic(_: &PanicInfo) -> ! {
 #[global_allocator]
 static ZEPHYR_ALLOCATOR: ZephyrAllocator = ZephyrAllocator;
 
-#[no_mangle]
-fn rust_main() {
-    unsafe {
-        msg("Hello from Rust\n\0".as_ptr().cast());
-        message("This is another message from Rust.");
-    }
-
-    panic!("Rust panic happened");
+pub type Result<T> = core::result::Result<T, Error>;
+#[derive(Debug)]
+pub enum Error {
+    Missing,
 }
 
-extern "C" {
-    fn msg(msg: *const c_char);
+#[no_mangle]
+fn rust_main() {
+    message("Beginning of Rust.");
+
+    let strip = Device::get_led_strip().expect("Getting led strip device");
+    message(&format!("Device present {}", strip.is_ready()));
+    message(&format!("LED device name: {:?}", strip.name()));
+
+    let mut strip = unsafe { LedStripDriver::unsafe_from_device(strip) };
+
+    // Program the LED just as an example.
+    let leds = [
+        LedRgb { r: 25, g: 25, b: 0 },
+        LedRgb { r: 0, g: 25, b: 25 },
+        LedRgb { r: 25, g: 0, b: 25 },
+    ];
+    loop {
+        for led in &leds {
+            strip.update_rgb(slice::from_ref(led));
+            zephyr::sleep(500);
+        }
+    }
+
+    // loop {}
 }
 
 mod zephyr {
     use alloc::ffi::CString;
+    use crate::zephyr_sys;
 
     // Print a basic message from a Rust string.  This isn't particularly
     // efficient, as the message will be heap allocated (and then immediately
@@ -46,10 +64,21 @@ mod zephyr {
             crate::zephyr_sys::msg_string(text.as_ptr());
         }
     }
+
+    // This is sort of safe.  There could be a time overflow, but it is memory
+    // safe, etc.
+    pub fn sleep(ms: u32) {
+        unsafe { zephyr_sys::c_k_sleep_ms(ms); }
+    }
+
 }
 
 mod zephyr_sys {
-    use core::{alloc::{GlobalAlloc, Layout}, ffi::c_char};
+    use core::{alloc::{GlobalAlloc, Layout}, ffi::{c_char, c_int, CStr}};
+
+    use alloc::{string::{String, ToString}, alloc::handle_alloc_error};
+
+    use crate::{Error, Result};
 
     extern "C" {
         pub fn c_k_panic() -> !;
@@ -60,6 +89,98 @@ mod zephyr_sys {
 
         // Log this message.
         pub fn msg_string(text: *const c_char);
+
+        // Device operations.
+        pub fn sys_device_is_ready(dev: *const ZDevice) -> c_int;
+
+        // Query for specific device.
+        pub fn get_led_strip() -> *mut ZDevice;
+
+        // Sleep in ms.
+        pub fn c_k_sleep_ms(ms: u32);
+    }
+
+    // The Underlying Zephyr `struct device`.
+    #[repr(C)]
+    pub struct ZDevice {
+        name: *const c_char,
+        config: *const u8,
+        api: *const u8,
+        state: *mut u8,
+        data: *mut u8,
+        #[cfg(zephyr = "CONFIG_DEVICE_DEPS")]
+        deps: *const u8,
+        // PM stuff.
+    }
+
+    // A Zephyr device.  To provide Rust ownership semantics, this struct keeps
+    // a private pointer to the Zephyr-side struct, which presumably has a
+    // static lifetime.
+    pub struct Device {
+        dev: *mut ZDevice,
+    }
+
+    impl Device {
+        pub fn get_led_strip() -> Result<Device> {
+            let dev = unsafe { get_led_strip() };
+            if dev.is_null() {
+                return Err(Error::Missing);
+            }
+            Ok(Device { dev })
+        }
+
+        pub fn is_ready(&self) -> bool {
+            unsafe { sys_device_is_ready(self.dev) != 0 }
+        }
+
+        // Get the name of the device from the device struct.
+        pub fn name(&self) -> String {
+            // Ick, probably not how we want to do this.
+            unsafe {
+                let dev = self.dev as *const ZDevice;
+                let name = CStr::from_ptr((*dev).name);
+                String::from_utf8_lossy(name.to_bytes()).to_string()
+            }
+        }
+    }
+
+    // Simple binding to the rgb scratch API.
+    #[repr(C)]
+    pub struct LedRgb {
+        #[cfg(zephyr = "CONFIG_LED_STRIP_RGB_SCRATCH")]
+        scratch: u8,
+        pub r: u8,
+        pub g: u8,
+        pub b: u8,
+    }
+
+    #[repr(C)]
+    struct LedStripDriverApi {
+        update_rgb: extern "C" fn(dev: *const ZDevice,
+                                  pixels: *const LedRgb,
+                                  num_pixels: c_size_t) -> c_int,
+        update_channels: extern "C" fn (dev: *const ZDevice,
+                                        channels: *const u8,
+                                        num_channels: c_size_t) -> c_int,
+    }
+
+    // An led strip driver.
+    pub struct LedStripDriver {
+        dev: *mut ZDevice,
+    }
+
+    impl LedStripDriver {
+        pub unsafe fn unsafe_from_device(dev: Device) -> LedStripDriver {
+            LedStripDriver { dev: dev.dev }
+        }
+
+        // The API.
+        pub fn update_rgb(&mut self, pixels: &[LedRgb]) -> c_int {
+            unsafe {
+                let api = (*self.dev).api as *const LedStripDriverApi;
+                ((*api).update_rgb)(self.dev, pixels.as_ptr(), pixels.len())
+            }
+        }
     }
 
     pub struct ZephyrAllocator;
@@ -72,7 +193,8 @@ mod zephyr_sys {
             // The picolibc/newlib malloc has an alignment of 8.  Any more than
             // this requires memalign, which isn't efficient.
             if align > 8 {
-                panic!("ZephyrAllocator, attempt at large alignment: {}", align);
+                handle_alloc_error(layout);
+                // panic!("ZephyrAllocator, attempt at large alignment: {}", align);
             }
 
             malloc(size)
--- zbbq/src/main.c
+++ zbbq/src/main.c
@@ -1,11 +1,19 @@
 /* My example */
 
+#include "zephyr/sys/mpsc_pbuf.h"
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log_ctrl.h>
 
 #define LOG_LEVEL 4
 #include <zephyr/logging/log.h>
 LOG_MODULE_REGISTER(main);
+
+#include <zephyr/drivers/led_strip.h>
+
+#define STRIP_NODE   DT_ALIAS(led_strip)
+#define STRIP_NUM_PIXELS  DT_PROP(DT_ALIAS(led_strip), chain_length)
+
+static const struct device *const strip = DEVICE_DT_GET(STRIP_NODE);
 
 static void wait_on_log_flushed(void)
 {
@@ -18,17 +26,22 @@ static void wait_on_log_flushed(void)
 	}
 }
 
-extern void rust_main(void);
-
-// Simple logging.
-void msg(const char* msg)
-{
-	LOG_INF("rust: %s", msg);
+const struct device *const get_led_strip(void) {
+	return strip;
 }
+
+extern void rust_main(void);
 
 void msg_string(const char* msg)
 {
 	LOG_INF("%s", msg);
+}
+
+// Syscall wrappers.  These are just generated as C wrappers and later we will
+// make this more efficient.
+bool sys_device_is_ready(const struct device *dev)
+{
+	return device_is_ready(dev);
 }
 
 /// Wrapper for k_panic(), simple way to get past all of the macros.
@@ -37,19 +50,48 @@ void c_k_panic()
 	k_panic();
 }
 
+/// Wrapper to sleep for n ms.
+void c_k_sleep_ms(uint32_t ms)
+{
+	k_sleep(K_MSEC(ms));
+}
+
 int main(void)
 {
-	LOG_INF("Hello world, from C main");
-	wait_on_log_flushed();
+	register uint32_t sp __asm__("sp");
+	extern uint32_t z_main_stack;
 
-	rust_main();
+	// The stack usage already shows the usage before we got to this point.
+	// To make this work a little better, clear out the stack, again.
+	uint32_t base = (uint32_t)&z_main_stack;
+	uint32_t len = sp - base;
 
-	// Log a periodic message.
-	int count = 0;
-	while (1) {
-		count++;
-		LOG_INF("Tick: %d", count);
-		wait_on_log_flushed();
-		k_sleep(K_MSEC(1000));
-	}
+	// Adjust the length to allow for the call to memset (and the base adjust).
+	len -= 20;
+	// Adjust the base to not overwrite the canary.
+	base += 4;
+	memset((void *)base, 0xaa, len);
+
+        // LOG_INF("sp: %08x", sp);
+        // LOG_INF("main: %08x", (uint32_t)&z_main_stack);
+
+        // LOG_INF("Hello world, from C main");
+        wait_on_log_flushed();
+
+        rust_main();
+        size_t left = 0;
+        k_thread_stack_space_get(k_current_get(), &left);
+        LOG_INF("after rust: %d bytes of stack used",
+                CONFIG_MAIN_STACK_SIZE - left);
+
+        // Log a periodic message.
+        /*
+        int count = 0;
+        while (1) {
+                count++;
+                LOG_INF("Tick: %d", count);
+                wait_on_log_flushed();
+                k_sleep(K_MSEC(1000));
+        }
+        */
 }
