027fc3091aa89f678064a83c906d734a8ea9130a
David Brown <davidb@davidb.org>
2024-03-08 11:57:11 -0700

    zbbq: Enable very basic HID keyboard supported
    Interface to the Zephyr USB stack primarily through C code, and provide two
    simple calls to this C code from Rust. One queries if all reports have been
    sent, meaning it is safe to send the next report, and the other sends a report.
    
    This simplifies the actual reports, not inserting any modifier keystrokes, but
    just setting the modifier bits themselves.  This does seem to work in Windows.

--- zbbq/src/lib.rs
+++ zbbq/src/lib.rs
@@ -1,7 +1,9 @@
 #![no_std]
 
-use alloc::{vec::Vec, string::ToString};
-use bbq_keyboard::{layout::LayoutManager, EventQueue, Event, KeyEvent};
+use core::ffi::c_int;
+
+use alloc::{vec::Vec, string::ToString, collections::VecDeque};
+use bbq_keyboard::{layout::LayoutManager, EventQueue, Event, KeyEvent, KeyAction};
 use zephyr::struct_timer;
 
 use crate::{matrix::Matrix, zephyr::Timer, devices::GpioFlags};
@@ -29,31 +31,96 @@ extern "C" fn rust_main () {
     };
 
     let mut layout = LayoutManager::new();
-    let mut silly = SillyQueue;
+    let mut events = VecDeque::new();
+
+    // Keys queued up to send to HID.
+    let mut keys = VecDeque::new();
 
     heartbeat.start(1);
     loop {
-        let mut events = Vec::new();
-
+        // Perform a single scan of the matrix.
         matrix.scan(|code, press| {
             // info!("Key {} {:?}", code, press);
             if press {
-                events.push(KeyEvent::Press(code));
+                events.push_back(Event::Matrix(KeyEvent::Press(code)));
+                // events.push(KeyEvent::Press(code));
             } else {
-                events.push(KeyEvent::Release(code));
+                events.push_back(Event::Matrix(KeyEvent::Release(code)));
+                // events.push(KeyEvent::Release(code));
             }
             Ok(())
         }).unwrap();
 
-        // Pass the keys off to the layout manager.
-        for event in events {
-            layout.handle_event(event, &mut silly);
+        // Push off any usb-hid keypresses.
+        usb_hid_push(&mut keys);
+
+        // Dispatch any events.
+        while let Some(event) = events.pop_front() {
+            match event {
+                Event::Matrix(key) => {
+                    // In the simple single-side case, matrix events are just
+                    // passed to the layout manager.
+                    layout.handle_event(key, &mut VecDeqEvents(&mut events));
+                }
+                Event::Key(key) => {
+                    // Keypress are queued up, to be sent to the hid layer.
+                    keys.push_back(key);
+                }
+
+                // Catch all for the rest.
+                event => info!("event: {:?}", event),
+            }
         }
 
-        layout.tick(&mut silly);
+        // Pass the keys off to the layout manager.
+        // for event in events {
+        //     layout.handle_event(event, &mut silly);
+        // }
+
+        layout.tick(&mut VecDeqEvents(&mut events));
 
         heartbeat.wait();
     }
+}
+
+/// Push a usb-hid event off to the USB stack, if that makes sense.
+fn usb_hid_push(keys: &mut VecDeque<KeyAction>) {
+    // If a report hasn't been sent, do nothing.
+    if unsafe {is_hid_accepting()} == 0 {
+        return;
+    }
+
+    if let Some(key) = keys.pop_front() {
+        match key {
+            KeyAction::KeyPress(code, mods) => {
+                let mut report = [0u8; 8];
+                report[7] = code as u8;
+                report[0] = mods.bits();
+                unsafe {hid_report(report.as_ptr())};
+            }
+            KeyAction::KeyRelease => {
+                let mut report = [0u8; 8];
+                unsafe {hid_report(report.as_ptr())};
+            }
+            KeyAction::KeySet(keys) => {
+                info!("TODO: KeySet");
+            }
+            KeyAction::ModOnly(mods) => {
+                let mut report = [0u8; 8];
+                report[0] = mods.bits();
+                unsafe {hid_report(report.as_ptr())};
+            }
+            KeyAction::Stall => {
+                // Not sure what this means with this interface.  For now, just
+                // go on a 1 ms tick.
+            }
+        }
+    }
+}
+
+extern "C" {
+    fn is_hid_accepting() -> c_int;
+    fn hid_report(report: *const u8);
 }
 
 pub type Result<T> = core::result::Result<T, Error>;
@@ -66,18 +133,20 @@ extern "C" {
     static mut heartbeat_timer: struct_timer;
 }
 
-// Silly event queue.
-struct SillyQueue;
+// An even queue built around a VecDeque
+struct VecDeqEvents<'a>(&'a mut VecDeque<Event>);
 
-impl EventQueue for SillyQueue {
+impl<'a> EventQueue for VecDeqEvents<'a> {
     fn push(&mut self, val: Event) {
-        match val {
-            Event::RawSteno(stroke) => {
-                // let text = stroke.to_string();
-                // info!("stroke: {}", text);
-                info!("stroke: {}", stroke.to_string());
-            }
-            ev => info!("event: {:?}", ev),
-        }
+        self.0.push_back(val);
+
+        // match val {
+        //     Event::RawSteno(stroke) => {
+        //         // let text = stroke.to_string();
+        //         // info!("stroke: {}", text);
+        //         info!("stroke: {}", stroke.to_string());
+        //     }
+        //     ev => info!("event: {:?}", ev),
+        // }
     }
 }
--- zbbq/src/main.c
+++ zbbq/src/main.c
@@ -4,8 +4,13 @@
 // bindings, there will be various pieces of code in this file to glue them
 // together, including invoking the `rust_main` entry.
 
+#include "zephyr/drivers/usb/usb_dc.h"
+#include "zephyr/usb/class/hid.h"
 #include <zephyr/kernel.h>
 #include <stdlib.h>
+
+#include <zephyr/usb/usb_device.h>
+#include <zephyr/usb/class/usb_hid.h>
 
 #define LOG_LEVEL 4
 #include <zephyr/logging/log.h>
@@ -13,7 +18,71 @@ LOG_MODULE_REGISTER(zbbq);
 
 extern void rust_main(void);
 
+K_SEM_DEFINE(usb_sem, 1, 1);
+const struct device *hid0_dev;
+
+// Callbacks from USB.
+static void in_ready_cb(const struct device *dev)
+{
+	// LOG_INF("HID in_ready");
+	k_sem_give(&usb_sem);
+}
+
+// TO RUST: Indicates if the USB-HID is accepting of a keypress.
+int is_hid_accepting(void) {
+	return k_sem_count_get(&usb_sem) > 0;
+}
+
+// TO RUST: Send the given report (8 bytes) to the hid interface.  Assumes that
+// `is_hid_accepting` is available (will stall if not).
+void hid_report(uint8_t *report) {
+	k_sem_take(&usb_sem, K_FOREVER);
+	hid_int_ep_write(hid0_dev, report, 8, NULL);
+}
+
+static const struct hid_ops ops = {
+	.int_in_ready = in_ready_cb,
+};
+
+// Use a basic keyboard HID report for boot mode.  As long as we aren't doing
+// NKRO, this should be adequate.
+static const uint8_t hid_kbd_report_desc[] = HID_KEYBOARD_REPORT_DESC();
+
+static void status_cb(enum usb_dc_status_code status, const uint8_t *param)
+{
+	LOG_INF("USB status: %d", status);
+}
+
+#define DEVICE_AND_COMMA(node_id) DEVICE_DT_GET(node_id),
+
 int main(void) {
+	const struct device *cdc_dev[] = {
+		DT_FOREACH_STATUS_OKAY(zephyr_cdc_acm_uart, DEVICE_AND_COMMA)
+	};
+	hid0_dev = device_get_binding("HID_0");
+	if (hid0_dev == NULL) {
+		LOG_ERR("Cannotr get USB HID 0 Device");
+		return 0;
+	}
+
+	for (int idx = 0; idx < ARRAY_SIZE(cdc_dev); idx++) {
+		if (!device_is_ready(cdc_dev[idx])) {
+			LOG_ERR("CDC ADM DEVICE %s is not ready",
+				cdc_dev[idx]->name);
+			return 0;
+		}
+	}
+
+	usb_hid_register_device(hid0_dev, hid_kbd_report_desc,
+				sizeof(hid_kbd_report_desc), &ops);
+	usb_hid_init(hid0_dev);
+
+	int ret = usb_enable(status_cb);
+	if (ret != 0) {
+		LOG_ERR("Failed to enable USB");
+		return 0;
+	}
+
 	rust_main();
 }
 
