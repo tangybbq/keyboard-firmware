d8dfe514fc08d49b4caf332f105837f4b7e368e1
David Brown <davidb@davidb.org>
2024-09-20 22:54:11 -0600

    jolt: Side select
    Bring in side select, fixing up the access to bbq-keyboard as well.

--- jolt/src/lib.rs
+++ jolt/src/lib.rs
@@ -9,7 +9,14 @@ use alloc::vec::Vec;
 
 use matrix::Matrix;
 use zephyr::printkln;
+use zephyr::sys::busy_wait;
 use zephyr::time::{Duration, sleep};
+use zephyr::raw::{
+    GPIO_INPUT,
+    GPIO_PULL_UP,
+};
+
+use bbq_keyboard::Side;
 
 mod matrix;
 
@@ -17,6 +24,19 @@ mod matrix;
 extern "C" fn rust_main() {
     printkln!("Hello world from Rust on {}",
               zephyr::kconfig::CONFIG_BOARD);
+
+    // Retrieve the side select.
+    let side_select = zephyr::devicetree::side_select::get_gpios();
+    let mut side_select = match side_select {
+        [pin] => pin,
+        // Compile error here means more than one pin is defined in DT.
+    };
+
+    side_select.configure(GPIO_INPUT | GPIO_PULL_UP);
+    busy_wait(5);
+    let side = if side_select.get() { Side::Left } else { Side::Right };
+
+    printkln!("Our side: {:?}", side);
 
     // Is this the best way to do this?  These aren't that big.
     let rows = zephyr::devicetree::aliases::matrix::get_rows();
