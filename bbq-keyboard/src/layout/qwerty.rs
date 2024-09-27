fb0785f6451e953d9c9020b06388571c73cb6ab3
David Brown <davidb@davidb.org>
2024-09-27 13:24:41 -0600

    bbq-keyboard: Increase combo time to 100ms
    In the qwerty layout, increase the time allowed for two keys of a combo
    to come in to 100ms.  Hopefully this will reduce the clumsy press times
    where I get extra keys, instead of a modifier.

--- bbq-keyboard/src/layout/qwerty.rs
+++ bbq-keyboard/src/layout/qwerty.rs
@@ -195,7 +195,7 @@ impl ComboHandler {
 
         self.pending_age += 1;
 
-        if self.pending_age >= 50 {
+        if self.pending_age >= 100 {
             self.push_pending();
         }
     }
