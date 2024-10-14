2fa4366cf1d97942d53f3c25cef31c5eec2740e8
David Brown <davidb@davidb.org>
2024-10-14 08:24:26 -0600

    dict-convert: Remove old dict convert code
    Now that bbq-tool supports dictionary conversion, remove the old tool.
    The format that this encoded to is no longer supported by the rest of
    the code.

--- dict-convert/build.rs
+++
@@ -1,7 +0,0 @@
-// Include revision information and time into the build.
-
-fn main() {
-    build_data::set_GIT_COMMIT();
-    build_data::set_GIT_DIRTY();
-    build_data::set_BUILD_TIMESTAMP();
-}
--- dict-convert/program.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/bash
-
-probe-rs download --chip RP2040 --protocol swd --format bin --base-address 0x10200000 main.bindict
