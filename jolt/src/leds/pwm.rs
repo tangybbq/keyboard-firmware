26139d91a00308b73696f51ee81caf1b528d9a24
David Brown <davidb@davidb.org>
2024-10-19 11:11:15 -0600

    jolt: Use zephyr Leds driver
    Remove the C wrappers for the PWM driver, and use the Leds driver from
    the zephyr crate.

--- jolt/src/leds/pwm.rs
+++ jolt/src/leds/pwm.rs
@@ -1,19 +1,19 @@
 //! Control of the LEDs.
 
+use zephyr::device::led::Leds;
+
 use super::LedGroup;
 
 pub struct PwmLed {
-    /// Number of LEDs managed.  There should be 3x this many LEDs in the device.
-    count: usize,
     /// Underlying device.
-    dev: *const zephyr::raw::device,
+    leds: Leds,
 }
 
 unsafe impl Send for PwmLed {}
 
 impl LedGroup for PwmLed {
     fn len(&self) -> usize {
-        self.count
+        self.leds.len() / 3
     }
 
     fn update(&mut self, values: &[rgb::RGB8]) {
@@ -21,11 +21,11 @@ impl LedGroup for PwmLed {
             let base = 3 * i;
             unsafe {
                 let value = (((values[0].r as u32) * 100) >> 8) as u8;
-                pwm_set_brightness(self.dev, (base + 0) as u32, value);
+                self.leds.set_brightness(base + 0, value).unwrap();
                 let value = (((values[0].g as u32) * 100) >> 8) as u8;
-                pwm_set_brightness(self.dev, (base + 1) as u32, value);
+                self.leds.set_brightness(base + 1, value).unwrap();
                 let value = (((values[0].b as u32) * 100) >> 8) as u8;
-                pwm_set_brightness(self.dev, (base + 2) as u32, value);
+                self.leds.set_brightness(base + 2, value).unwrap();
             }
         }
     }
@@ -33,30 +33,13 @@ impl LedGroup for PwmLed {
 
 impl PwmLed {
     pub fn get_instance() -> Option<PwmLed> {
-        let leds = unsafe { get_pwm() };
-        let count = leds.count / 3;
-        if count > 0 {
-            Some(PwmLed {
-                count: count as usize,
-                dev: leds.dev,
-            })
+        // TODO: Use a chosen and be conditional.
+        let leds = zephyr::devicetree::pwm_leds::get_instance()?;
+        // Require at least 3 LEDs.
+        if leds.len() >= 3 {
+            Some(PwmLed { leds })
         } else {
             None
         }
     }
-}
-
-#[repr(C)]
-#[allow(non_camel_case_types)]
-struct pwm_led_info {
-    dev: *const zephyr::raw::device,
-    count: u32,
-}
-
-extern "C" {
-    fn get_pwm() -> pwm_led_info;
-    fn pwm_set_brightness(dev: *const zephyr::raw::device,
-                          index: u32,
-                          value: u8)
-        -> core::ffi::c_int;
 }
