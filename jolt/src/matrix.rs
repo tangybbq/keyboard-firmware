fa14a68ceafbf65d51aedad35367e3d1a6e6bc10
David Brown <davidb@davidb.org>
2024-10-22 12:01:28 -0600

    jolt: matrix: spread out matrix scanning
    To reduce CPU load, spread out the matrix scanning to one column per 1ms
    tick.  This has the advantage of using less CPU.  But it also reduces
    the debouncing to only 4 samples.  Try this for a while, and if I see
    any bouncing (or delayed keys), revert this, as the CPU burden isn't
    actually all that great.

--- jolt/src/matrix.rs
+++ jolt/src/matrix.rs
@@ -21,41 +21,57 @@ pub struct Matrix {
     rows: Vec<GpioPin>,
     cols: Vec<GpioPin>,
     state: Vec<Debouncer>,
-    side: Side
+    side: Side,
+
+    // What column are we scanning.
+    column: usize,
 }
 
 impl Matrix {
     pub fn new(rows: Vec<GpioPin>, cols: Vec<GpioPin>, side: Side) -> Matrix {
-        let state = (0 .. rows.len() * cols.len()).map(|_| Debouncer::new()).collect();
+        let state = (0 .. rows.len() * cols.len()).map(|i| Debouncer::new(i)).collect();
         let token = unsafe { GpioToken::get_instance().unwrap() };
-        let mut result = Matrix { token, rows, cols, state, side };
+        let mut result = Matrix { token, rows, cols, state, side, column: 0 };
         Self::pin_setup(&mut result.token, &mut result.cols, &mut result.rows);
         result
     }
 
     /// Perform a single scan of the matrix, calling `act` for every key that changes.
+    ///
+    /// To avoid using quite as much CPU on the Matrix scan, only scan a single column each tick.
     pub fn scan<F>(&mut self, mut act: F)
         where F: FnMut(u8, bool),
     {
         let bias = if self.side.is_left() { 0 } else { self.state.len() };
-        let mut states = self.state.iter_mut().enumerate();
-        for col in &mut self.cols {
-            unsafe { col.set(&mut self.token, true); }
-            unsafe { busy_wait(5); }
-            for row in &mut self.rows {
-                let (code, state) = states.next().unwrap();
-                match state.react(unsafe { row.get(&mut self.token) }) {
-                    KeyAction::Press => {
-                        act((code + bias) as u8, true);
-                    }
-                    KeyAction::Release => {
-                        act((code + bias) as u8, false);
-                    }
-                    _ => (),
-                }
+        let mut states = self.state.iter_mut()
+            // It is important to enumerate before skipping so that the numbers are still correct.
+            .enumerate()
+            .skip(self.column * self.rows.len());
+        let col = &mut self.cols[self.column];
+        unsafe { col.set(&mut self.token, true); }
+        unsafe { busy_wait(5); }
+        for row in &mut self.rows {
+            let (code, state) = states.next().unwrap();
+            if code != state.index {
+                panic!("Mismatch on scan, key {} vs {}", code, state.index);
             }
-            unsafe { col.set(&mut self.token, false); }
-            // busy_wait(5);
+            match state.react(unsafe { row.get(&mut self.token) }) {
+                KeyAction::Press => {
+                    act((code + bias) as u8, true);
+                }
+                KeyAction::Release => {
+                    act((code + bias) as u8, false);
+                }
+                _ => (),
+            }
+        }
+        unsafe { col.set(&mut self.token, false); }
+        // busy_wait(5);
+
+        // Advance to the next tick.
+        self.column += 1;
+        if self.column >= self.cols.len() {
+            self.column = 0;
         }
     }
 
@@ -100,15 +116,22 @@ struct Debouncer {
     state: KeyState,
     /// Count how many times we've seen a given debounce state.
     counter: usize,
+
+    /// For debugging, put the index of the debouncer here.
+    index: usize,
 }
 
-const DEBOUNCE_COUNT: usize = 20;
+// This is the count of steps to debounce.  This really needs to be defined in terms of the number
+// of columns in the keyboard.  For now, just define based on my current keyboard which happen to
+// always have 6 rows.
+const DEBOUNCE_COUNT: usize = 20_usize.div_ceil(6);
 
 impl Debouncer {
-    fn new() -> Debouncer {
+    fn new(index: usize) -> Debouncer {
         Debouncer {
             state: KeyState::Stable(false),
             counter: 0,
+            index,
         }
     }
 
