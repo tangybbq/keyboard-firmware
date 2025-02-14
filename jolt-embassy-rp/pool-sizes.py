84d2c05081db61dcb87689f590067c4502e41850
David Brown <davidb@davidb.org>
2025-02-14 09:17:06 -0700

    jolt-embassy-rp: Update pool-sizes.py
    Format the output better, also showing the individual tasks.

--- jolt-embassy-rp/pool-sizes.py
+++ jolt-embassy-rp/pool-sizes.py
@@ -4,15 +4,18 @@ import subprocess
 import re
 
 output = subprocess.run(
-        ["cargo", "+nightly", "nm", "--features"," nightly", "--", "-S"],
+        ["cargo", "+nightly", "nm", "--features"," nightly", "--", "-S", "--size-sort"],
         text=True, capture_output=True
 ).stdout
 
 total_size = 0
 for line in output.splitlines():
-    match = re.search(r"^[0-9a-fA-F]+ ([0-9a-fA-F]+)\s+.*::POOL::", line)
+    match = re.search(r"^[0-9a-fA-F]+ ([0-9a-fA-F]+)\s+.*:(\S+)::POOL::", line)
     if match:
         size = int(match.group(1), 16)
+        name = match.group(2)
+        print(f"{size:5d} {name}")
         total_size += size
 
-print(f"Total POOL size: {total_size} bytes")
+print("-----")
+print(f"{total_size:5d} Total bytes")
